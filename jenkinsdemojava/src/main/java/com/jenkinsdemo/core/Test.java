0f1003b91babe498195f1530cbd65f697b411f1e
sachin06192 <33274650+sachin06192@users.noreply.github.com>
2024-05-12 13:11:16 +0530

    Update Test.java

--- jenkinsdemojava/src/main/java/com/jenkinsdemo/core/Test.java
+++ jenkinsdemojava/src/main/java/com/jenkinsdemo/core/Test.java
@@ -3,6 +3,7 @@ package com.jenkinsdemo.core;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-		System.out.println("Hello, World, Feature Branch!");
+	System.out.println("Hello, World, Feature Branch!");
+	System.out.println("Hello, World, Test auto trigger !");    
     }
 }
