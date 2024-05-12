94b60c4159e15f79e9e422eb89fe971b57a46d5c
Sachin <sachin06192@gmail.com>
2024-05-12 11:31:08 +0530

    updates for mvn

--- jenkinsdemojava/src/main/java/com/jenkinsdemo/core/Test.java
+++ jenkinsdemojava/src/main/java/com/jenkinsdemo/core/Test.java
@@ -1,4 +1,6 @@
-public class HelloWorld {
+package com.jenkinsdemo.core;
+
+public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 		System.out.println("Hello, World, Feature Branch!");
